959e1cef4ee097542ba5d319058fc0c3ef9ac639
cbcarlos07 <cbcarlos08@gmail.com>
2017-05-18 20:27:21 -0400

    Curso de Android

--- app/src/main/java/tudolist/cursoandroid/com/tudolist/MainActivity.java
+++ app/src/main/java/tudolist/cursoandroid/com/tudolist/MainActivity.java
@@ -1,13 +1,23 @@
 package tudolist.cursoandroid.com.tudolist;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    private Button botaoAdicionar;
+    private ListView listView;
+    private EditText texto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        botaoAdicionar = (Button) findViewById(R.id.botaoAdicionarId);
+        listView       = (ListView) findViewById(R.id.listViewId);
+        texto          = (EditText) findViewById(R.id.textoId);
+
     }
 }
